320614eb00327e87ce98480b63ba820f84c04f4e
dimagong <dimagong@gmail.com>
2024-07-22 19:10:46 +0200

    feat(article): rework api for gettinf list of articles with new data base

--- services/articles.js
+++ services/articles.js
@@ -9,24 +9,57 @@ import {
 	getDocs,
 	updateDoc,
 	deleteDoc,
+	collectionGroup,
 } from "firebase/firestore"
-import { ArticleId } from "./../models/articleModel.js"
+import { ArticleId, ArticlesContent, ArticleEntity } from "./../models/articleModel.js"
 
 export const articles = async (req, res) => {
 	const articlesRef = adminDB.collection("articles")
+
 	try {
-		const snapshot = await articlesRef.get()
-		const data = snapshot.docs.map((doc) => ({
-			...new ArticleId(
-				doc.data().id,
-				doc.data().title,
-				doc.data().subtitle,
-				doc.data().content,
-				doc.data().url
-			),
-		}))
-		console.log(data)
-		return res.status(201).json(data)
+		// const snapshotArticles = await articlesRef.get()
+		// const snapshotArticlesDocs = snapshotArticles.docs
+		// const data = snapshotArticlesDocs.map((doc) => {
+		// 	return {
+		// 		...new ArticleId(doc.data().id, doc.data().title, doc.data().subtitle, doc.data().url),
+		// 	}
+		// })
+
+		const listArticlesRefs = await articlesRef.get()
+
+		const listCollectionsDocs = listArticlesRefs.docs
+
+		const docIdx = listCollectionsDocs.map((doc) => doc.id)
+
+		const collectContent = async () => {
+			const mapContent = await Promise.all(
+				docIdx.map(async (id) => {
+					const docById = articlesRef.doc(id)
+					const collectionsDocId = await docById.listCollections()
+					//let content = { id, data: null }
+					let content = { ...new ArticlesContent(id, null) }
+					const dataContent = await Promise.all(
+						collectionsDocId.map(async (el) => {
+							const snapshot = await el.get()
+							let snapshotData = []
+							snapshot.forEach((doc) => {
+								// snapshotData.push(doc.data())
+								snapshotData.push({ ...new ArticleEntity(doc.data().question, doc.data().answer) })
+							})
+							return snapshotData
+						})
+					)
+					content.data = [...dataContent]
+					return content
+				})
+			)
+
+			return mapContent
+		}
+
+		const completeContent = await collectContent()
+		// console.log("competeContent  =>", completeContent)
+		return res.status(200).json(completeContent)
 	} catch (error) {
 		return res.status(500).json({ general: "Something went wrong, please try again" })
 	}
